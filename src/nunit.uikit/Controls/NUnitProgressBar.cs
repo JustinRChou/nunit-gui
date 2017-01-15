c46633a943bb02fd10b9e9eb8bdb048d2b55a378
JustinRChou <justinrchou@gmail.com>
2017-01-15 00:09:57 -0800

    Add Percent Text to Progress Bar

--- src/nunit.uikit/Controls/NUnitProgressBar.cs
+++ src/nunit.uikit/Controls/NUnitProgressBar.cs
@@ -37,21 +37,37 @@ namespace NUnit.UiKit.Controls
 
     public class NUnitProgressBar : ProgressBar
     {
-        public readonly static Color[][] BrushColors = 
+        public readonly static Color[][] BrushColors =
         {
               new Color[] { Color.FromArgb(32, 205, 32), Color.FromArgb(16, 64, 16) },  // Success
               new Color[] { Color.FromArgb(255, 255, 0), Color.FromArgb(242, 242, 0) }, // Warning
               new Color[] { Color.FromArgb(255, 0, 0), Color.FromArgb(150, 0, 0) }      // Failure
         };
-        
+
         private Brush _brush;
+
+        private Font _font;
+
+        private int _curHeight; // Height changes between constructor and OnPaint. Storing it to detect change so the font size could be recalculated.
 
         public NUnitProgressBar()
         {
             this.SetStyle(ControlStyles.UserPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint, true);
-
+            _curHeight = this.Height ;
             _status = TestProgressBarStatus.Success;
             _brush = CreateBrush(_status);
+            _font = new Font("Arial", 1);
+            //FontFamily fontFamily = new Font("Arial",1).FontFamily;
+            //int ascent = fontFamily.GetCellAscent(FontStyle.Regular);
+            //int emHeight = fontFamily.GetEmHeight(FontStyle.Regular);
+            //int pixel =5;
+            //_font.Size.s
+            //float Size = (pixel  * emHeight) / ascent;
+            _font = new Font(_font.FontFamily, CalculateFontSize());
+   //         _font.S
+   //         ascentPixel =
+   //font.Size * ascent / fontFamily.GetEmHeight(FontStyle.Regular);
+   //         int ascent = _font.FontFamily.GetCellAscent(FontStyle.Regular);
         }
 
         #region Properties
@@ -66,7 +82,7 @@ namespace NUnit.UiKit.Controls
                 {
                     _status = value;
 
-                    if (_brush != null) 
+                    if (_brush != null)
                         _brush.Dispose();
                     _brush = CreateBrush(value);
                 }
@@ -83,19 +99,33 @@ namespace NUnit.UiKit.Controls
             rec.Inflate(-2, -2);
             if (ProgressBarRenderer.IsSupported)
                 ProgressBarRenderer.DrawHorizontalBar(e.Graphics, rec);
+            double percent = ((double)Value / Maximum);
             rec.Inflate(-1, -1);
-            rec.Width = (int)(rec.Width * ((double)Value / Maximum));
+            rec.Width = (int)(rec.Width * percent);
             e.Graphics.FillRectangle(_brush, rec); //2, 2, rec.Width, rec.Height);
+            string strPercent = percent.ToString("P2");
+            if (_curHeight != rec.Height) {
+                _curHeight = rec.Height;
+                _font = new Font(_font.FontFamily,CalculateFontSize());
+            }
+            SizeF strSize= e.Graphics.MeasureString(strPercent,_font);
+            e.Graphics.DrawString(strPercent, _font, new SolidBrush(Color.Black), (this.ClientRectangle.Width- strSize.Width) / 2, rec.Y);
         }
 
         private Brush CreateBrush(TestProgressBarStatus status)
         {
             Color[] colors = BrushColors[(int)status];
             return new LinearGradientBrush(
-                new Point(0, 0), 
-                new Point(0, this.ClientSize.Height - 3), 
-                colors[0], 
+                new Point(0, 0),
+                new Point(0, this.ClientSize.Height - 3),
+                colors[0],
                 colors[1]);
+        }
+
+        private int CalculateFontSize() {
+            int ascent = _font.FontFamily.GetCellAscent(FontStyle.Regular);
+            int emHeight = _font.FontFamily.GetEmHeight(FontStyle.Regular);
+            return ((int)(_curHeight*0.9f) * emHeight) / ascent;
         }
 
         #endregion
